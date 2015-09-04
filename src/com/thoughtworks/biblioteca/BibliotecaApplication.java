430ae16920276c2c3aa054e29ce1c9e5513a574d
Amandeep Singh <amandees@thoughtworks.com>
2015-09-04 12:55:01 +0530

    Refactored the code.Now all methods in bibliotecaapplication other than start are private and writing spec for only start and asserting output from others on it

--- src/com/thoughtworks/biblioteca/BibliotecaApplication.java
+++ src/com/thoughtworks/biblioteca/BibliotecaApplication.java
@@ -1,23 +1,28 @@
 package com.thoughtworks.biblioteca;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BibliotecaApplication {
 
+    private final List<String> listOfBooks = Arrays.asList("scjp", "head first java");
+
     public static void main(String[] args) {
         BibliotecaApplication bibliotecaApplication = new BibliotecaApplication();
-        bibliotecaApplication.start();
+        bibliotecaApplication.start("Hey! Welcome to biblioteca");
     }
 
-    public void start() {
-        printWelcomeMessage("Hey! Welcome to biblioteca");
+    public void start(String welcomeMessage) {
+        printWelcomeMessage(welcomeMessage);
+        System.out.println("List of books we have is: ");
+        printBooks(listOfBooks);
     }
 
-    public void printWelcomeMessage(String welcomeMessage) {
+    private void printWelcomeMessage(String welcomeMessage) {
         System.out.println(welcomeMessage);
     }
 
-    public void printBooks(List<String> books) {
+    private void printBooks(List<String> books) {
         System.out.println(books);
     }
 }
--- test/com/thoughtworks/biblioteca/BibliotecaApplicationTest.java
+++ test/com/thoughtworks/biblioteca/BibliotecaApplicationTest.java
@@ -4,47 +4,21 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 public class BibliotecaApplicationTest {
 
     private BibliotecaApplication bibliotecaApplication;
 
     @Test
-    public void shouldPrintWelcomeMessageOnTheScreen() {
+    public void shouldPrintWelcomeMessageOnTheScreenAndTheListOfBooks() {
         bibliotecaApplication = new BibliotecaApplication();
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        bibliotecaApplication.printWelcomeMessage("welcome");
+        bibliotecaApplication.start("welcome");
 
-        assertEquals("welcome\n", outContent.toString());
-    }
-
-    @Test
-    public void shouldCallPrintWelcomeMessageWhenICallStart() {
-        bibliotecaApplication = spy(new BibliotecaApplication());
-
-        bibliotecaApplication.start();
-
-        verify(bibliotecaApplication, times(1)).printWelcomeMessage("Hey! Welcome to biblioteca");
-    }
-
-    @Test
-    public void shouldPrintListOfBooksOnTheScreen() {
-        List<String> books = Arrays.asList("first book", "second book");
-        bibliotecaApplication = new BibliotecaApplication();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        bibliotecaApplication.printBooks(books);
-
-        assertEquals("[first book, second book]\n", outContent.toString());
+        assertEquals("welcome\n[scjp, head first java]\n", outContent.toString());
     }
 }
