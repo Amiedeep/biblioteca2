dd748ee4f63dfa30731c9932861a672359c95056
Amandeep Singh <amandees@thoughtworks.com>
2015-09-09 23:01:07 +0530

    [Release 1, Return Book] Adding new option return book in list of options and wrinting spec for it

--- src/com/thoughtworks/biblioteca/Options.java
+++ src/com/thoughtworks/biblioteca/Options.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Options {
 
-    private final List<String> options = Arrays.asList("List Books", "Checkout book", "Quit");
+    private final List<String> options = Arrays.asList("List Books", "Checkout book", "Quit", "Return book");
 
     public String getOptions() {
         String listOfOptions = "Please choose one option: \n";
