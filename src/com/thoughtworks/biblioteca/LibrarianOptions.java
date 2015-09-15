929a4a83d83270f30b08132175125dc31ba9a73f
Amandeep Singh <amandees@thoughtworks.com>
2015-09-15 23:21:34 +0530

    [Release 2, User Accounts] Changed the order of options in GuestUserOptions class LibrarianOptions class

--- src/com/thoughtworks/biblioteca/LibrarianOptions.java
+++ src/com/thoughtworks/biblioteca/LibrarianOptions.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class LibrarianOptions extends Options {
 
     public LibrarianOptions() {
-        super(Arrays.asList("List Books", "Checkout book", "Quit", "Return book", "List Movies", "checkout movie", "Books status"));
+        super(Arrays.asList("List Books", "List Movies", "checkout movie", "Checkout book", "Return book", "Books status", "Quit"));
     }
 }
