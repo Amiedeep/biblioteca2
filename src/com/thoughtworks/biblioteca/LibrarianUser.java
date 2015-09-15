bd6034ad8b2b99f7e8ad2ea97aca81aadf4348d2
Amandeep Singh <amandees@thoughtworks.com>
2015-09-15 15:18:26 +0530

    [Release 2, User Accounts] Added a LibrarianUser class which implements User interface and implemented the logIn behaviour to return me false when i pass null as userName and password

---
+++ src/com/thoughtworks/biblioteca/LibrarianUser.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.biblioteca;
+
+public class LibrarianUser implements User {
+
+    private String userName;
+    private String password;
+
+    public LibrarianUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    public boolean logIn(String thatUserName, String thatPassword) {
+        return false;
+    }
+}
---
+++ test/com/thoughtworks/biblioteca/LibrarianUserTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+
+public class LibrarianUserTest {
+
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new LibrarianUser("222-1111", "dcba");
+    }
+
+    @Test
+    public void shouldReturnMeFalseWhenUserNameAndPasswordAreNull() {
+        assertFalse(user.logIn(null, null));
+    }
+}
