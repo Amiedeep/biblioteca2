16cf456a96d4517461cb6e93659bcee1eedc1b94
Amandeep Singh <amandees@thoughtworks.com>
2015-09-17 17:15:44 +0530

    [Release 2, User Accounts] Added new option for logout for all the users

--- src/com/thoughtworks/options/GuestUserOptions.java
+++ src/com/thoughtworks/options/GuestUserOptions.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class GuestUserOptions extends Options {
 
     public GuestUserOptions() {
-        super(Arrays.asList("List Books", "List Movies", "checkout movie", "quit"));
+        super(Arrays.asList("List Books", "List Movies", "checkout movie", "quit", "logout"));
     }
 }
